edc8d5b6dd85f4ecec361fb2f9262b2c4f557d6d
Marc Ribalta Gene <marc.ribalta@local.eurecat.org>
2018-10-27 16:03:55 +0200

    added the routes file

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+export const routes: Routes = [
+  { path: '', component:  ''}
+];
+
+@NgModule({
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
