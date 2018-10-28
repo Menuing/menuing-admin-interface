845484db8154868215ba4aa8f6027a2007feb522
Marc Ribalta Gene <marc.ribalta@eurecat.org>
2018-10-28 17:23:38 +0100

    added the modify recipe part

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { LoginComponent } from './login/login.component';
 import { RecipeCheckComponent } from './recipe/recipe-check/recipe-check.component';
 import { RecipeDetailComponent } from './recipe/recipe-detail/recipe-detail.component';
 import { AddRecipeComponent } from './recipe/add-recipe/add-recipe.component';
+import { ModifyRecipeComponent } from './recipe/modify-recipe/modify-recipe.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: 'login', pathMatch: 'full'},
   {path: 'login', component: LoginComponent},
   {path: 'recipes', component: RecipeCheckComponent},
   {path: 'recipe-detail', component: RecipeDetailComponent},
-  {path: 'recipe-add', component: AddRecipeComponent}
+  {path: 'recipe-add', component: AddRecipeComponent},
+  {path: 'recipe-modify', component: ModifyRecipeComponent}
 ];
 
 @NgModule({
