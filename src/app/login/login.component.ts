d694a7c0131ce9b5cf3686ce41d73a5c065824df
Alba Lamas <alba.v.lamas@gmail.com>
2018-11-13 20:21:22 +0100

    ir works, but not at all

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 import {ActivatedRoute} from '@angular/router';
 import { LoginService } from './login.service';
-import { Login } from './login';
+import { Admin } from './admin';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -9,22 +11,36 @@ import { Login } from './login';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  user: Login;
+  admin: Admin;
   errorMessage = 'This user doesn\'t exist or bad credentials';
+  loginForm: FormGroup;
+  receivedAdmin:Admin;
 
-  constructor(private route: ActivatedRoute, private loginService: LoginService) { }
-
+  constructor(private fb:FormBuilder, private route:ActivatedRoute, private router: Router, private loginService: LoginService){}
+    
   ngOnInit() {
-    var id = this.route.params['value'].id;
-    this.loginService.getUser(username)
-      .subscribe(
-        (user: Login) => {
-          this.user = user;
-        },
-        error => this.errorMessage = <any>error.message);
+    console.log("entra");
+    this.loginForm = this.fb.group({
+      'username': ['',  Validators.required],
+      'password': ['',  Validators.required]
+    });
   }
 
-  login(username, pwd) {
+  login():void {
+    console.log("wtf");
+    this.admin = new Admin(this.loginForm.getRawValue);
+    this.loginService.getUser(this.admin.username)
+      .subscribe(
+        (admin: Admin) => {
+          this.receivedAdmin = admin;
+        },
+        error => this.errorMessage = <any>error.message);
+    if(this.admin.password === this.receivedAdmin.password){
+      alert("wrong");
+      return;
+    }   
+
+    alert("sccess!!!");    
   }
 
 }
