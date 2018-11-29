a730f8c8de7b994f3ea476447dd234334f0c4791
Alba Lamas <alba.v.lamas@gmail.com>
2018-11-29 20:24:42 +0100

    filter recipes

--- src/app/recipe/add-recipe/add-recipe.component.ts
+++ src/app/recipe/add-recipe/add-recipe.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {Recipe} from '../Recipe';
+import {Recipe} from '../recipe';
 import {Router} from '@angular/router';
 import {RecipeService} from '../recipe.service';
 
